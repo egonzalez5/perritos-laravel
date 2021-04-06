077a92733eef5cc31fc7c50ce20e65329359b649
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-04-06 17:15:29 -0400

    apps

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://perritos-laravel.herokuapp.com/'),
+    'url' => env('APP_URL', 'https'),
 
     'asset_url' => env('ASSET_URL', null),
 
